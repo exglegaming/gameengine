06cf47e8b9f50fdba464f0ba0c6af4cbb0fa4a9c
ExgleGaming <EaglesFan91x@outlook.com>
2025-11-18 22:11:28 -0500

    SDL Delay

--- src/Game.cpp
+++ src/Game.cpp
@@ -72,9 +72,12 @@ void Game::Setup() {
 
 void Game::Update() {
     // If we are too fast, waste time until we reach the MILLISECS_PER_FRAME
-    while (!SDL_TICKS_PASSED(SDL_GetTicks(), millisecsPreviousFrame + MILLISECS_PER_FRAME));
+    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
+    if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+        SDL_Delay(timeToWait);
+    }
 
-    // store teh current frame time
+    // store the current frame time
     millisecsPreviousFrame = SDL_GetTicks();
 
     playerPosition.x += playerVelocity.x;
