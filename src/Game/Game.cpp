512fa3350e1150807695c2ae858ca4fdecca43f9
ExgleGaming <EaglesFan91x@outlook.com>
2026-03-22 20:14:00 -0400

    ECS folder and file structure setup

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include "../Logger/Logger.h"
+#include "../ECS/ECS.h"
 #include <SDL2/SDL_image.h>
 #include <glm/glm.hpp>
 #include <iostream>
@@ -43,9 +44,11 @@ void Game::Initialize() {
         return;
     }
     // SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-	SDL_SetWindowFullscreen(window, 0);
-	SDL_SetWindowSize(window, 1280, 720);
-	SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+	
+	// Make Window windowed
+	// SDL_SetWindowFullscreen(window, 0);
+	// SDL_SetWindowSize(window, 1280, 720);
+	// SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 
     isRunning = true;
 }
@@ -66,12 +69,12 @@ void Game::ProcessInput() {
     }
 }
 
-glm::vec2 playerPosition;
-glm::vec2 playerVelocity;
-
 void Game::Setup() {
-    playerPosition = glm::vec2(10.0, 20.0);
-    playerVelocity = glm::vec2(100.0, 0.0);
+    // TODO:
+	// Entity tank = registry.CreateEntity
+	// tank.AddComponent<TransformComponent>();
+	// tank.AddComponent<BoxColliderComponent>();
+	// tank.AddComponet<SpriteComponent>("./assets/images/tank.png");
 }
 
 void Game::Update() {
@@ -87,28 +90,17 @@ void Game::Update() {
     // store the "previous" frame time
     millisecsPreviousFrame = SDL_GetTicks();
 
-    playerPosition.x += playerVelocity.x * deltaTime;
-    playerPosition.y += playerVelocity.y * deltaTime;
+    // TODO:
+	// MovementSystem.Update();
+	// CollisionSystem.Update();
+	// DamageSystem.Update();
 }
 
 void Game::Render() {
     SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
     SDL_RenderClear(renderer);
 
-    // Loads a PNG texture
-    SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-
-    // What is the destingation rectangle that we want to place our texture
-    SDL_Rect dstRect = {
-        static_cast<int>(playerPosition.x),
-        static_cast<int>(playerPosition.y),
-        32,
-        32
-    };
-    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
-    SDL_DestroyTexture(texture);
+    // TODO: Render game objects...
 
     SDL_RenderPresent(renderer);
 }
