7b7f1732a6fc7c5e0bb039c6466912cafbda181d
ExgleGaming <EaglesFan91x@outlook.com>
2026-03-22 20:40:29 -0400

    System Component Signature

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -1,15 +1,43 @@
 #pragma once
 
+#include <bitset>
+#include <vector>
+
+const unsigned int MAX_COMPONENTS = 32;
+
+// Signature
+// We use a bitset (1s and 0s) to keep track of which components an entity has,
+// and also helps keep track of which entities a system is interested in.
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
 class Component {
 	// TODO:...
 };
 
 class Entity {
-	// TODO:...
+	private:
+		int id;
+
+	public:
+		Entity(int id): id(id) {};
+		int GetId() const;
+		//...
 };
 
+// The system processes entities that contain a specific signature
 class System {
-	// TODO:...
+	private:
+		Signature componentSignature;
+		std::vector<Entity> entities;
+
+	public:
+		System() = default;
+		~System() = default;
+
+		void AddEntityToSystem(Entity entity);
+		void RemoveEntityFromSystem(Entity entity);
+		std::vector<Entity> GetSystemEntities() const;
+		Signature& GetComponentSignature() const;
 };
 
 class Registry {
