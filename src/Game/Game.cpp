4b4fb56c6d7b68321246bb879531856836ed0218
ExgleGaming <EaglesFan91x@outlook.com>
2026-05-30 01:11:33 -0400

    display textures in our renderer system

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -89,19 +89,19 @@ void Game::setup()
 	registry->add_system<RenderSystem>();
 
 	// Adding assets to the asset store
-	asset_store->add_texture(renderer, "tank-image", "./assets/images/tank-panther-right.png");
-	asset_store->add_texture(renderer, "truck-image", "./assets/images/truck-ford-right.png");
+	asset_store->add_texture(renderer, "tank-image", "../assets/images/tank-panther-right.png");
+	asset_store->add_texture(renderer, "truck-image", "../assets/images/truck-ford-right.png");
 
     // Create an entity
 	Entity tank = registry->create_entity();
-	tank.add_component<TransformComponent>(glm::vec2(10.0, 30.0), glm::vec2(1.0, 1.0), 0.0);
+	tank.add_component<TransformComponent>(glm::vec2(10.0, 10.0), glm::vec2(1.0, 1.0), 0.0);
 	tank.add_component<RigidBodyComponent>(glm::vec2(40.0, 0.0));
-	tank.add_component<SpriteComponent>("tank-image", 10, 10);
+	tank.add_component<SpriteComponent>("tank-image", 32, 32);
 
 	Entity truck = registry->create_entity();
 	truck.add_component<TransformComponent>(glm::vec2(50.0, 100.0), glm::vec2(1.0, 1.0), 0.0);
 	truck.add_component<RigidBodyComponent>(glm::vec2(0.0, 50.0));
-	truck.add_component<SpriteComponent>("truck-image", 10, 50);
+	truck.add_component<SpriteComponent>("truck-image", 32, 32);
 }
 
 void Game::update() 
@@ -132,7 +132,7 @@ void Game::render()
     SDL_RenderClear(renderer);
 
     // Invoke all the systems that need to update
-	registry->get_system<RenderSystem>().update(renderer);
+	registry->get_system<RenderSystem>().update(renderer, asset_store);
 
     SDL_RenderPresent(renderer);
 }
--- src/Systems/RenderSystem.h
+++ src/Systems/RenderSystem.h
@@ -4,6 +4,7 @@
 #include "../ECS/ECS.h"
 #include "../Components/TransformComponent.h"
 #include "../Components/SpriteComponent.h"
+#include "../AssetStore/AssetStore.h"
 #include <SDL2/SDL.h>
 
 class RenderSystem : public System 
@@ -15,13 +16,34 @@ class RenderSystem : public System
 			require_component<SpriteComponent>();
 		}
 
-		void update(SDL_Renderer* renderer) 
+		void update(SDL_Renderer* renderer, std::unique_ptr<AssetStore>& asset_store) 
 		{
 			// Loop all entities that the system is interested in
 			for (auto entity: get_system_entities()) {
-				// Upadate entity position based on its velocity
+				// Update entity position based on its velocity
 				const auto transform = entity.get_component<TransformComponent>();
 				const auto sprite = entity.get_component<SpriteComponent>();
+
+				// Set the source rectangle of our original sprite texture
+				SDL_Rect src_rect = sprite.src_rect;
+
+				// Set the destination rectangle with the x, y positiong to be rendered
+				SDL_Rect dest_rect = {
+					static_cast<int>(transform.position.x),
+					static_cast<int>(transform.position.y),
+					static_cast<int>(sprite.width * transform.scale.x),
+					static_cast<int>(sprite.height * transform.scale.y)
+				};
+
+				SDL_RenderCopyEx(
+					renderer,
+					asset_store->get_texture(sprite.asset_id),
+					&src_rect,
+					&dest_rect,
+					transform.rotation,
+					NULL,
+					SDL_FLIP_NONE
+				);
 
 				// TODO: Draw the PNG texture
 			}
